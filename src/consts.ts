166605e32ccaf0c550da61c937b91fff66ba16ff
UnionPan <yp1170@nyu.edu>
2025-07-25 15:48:26 -0400

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Adam Melnik';
+export const SITE_DESCRIPTION = 'Lighting Designer & Passionate Photographer - Creating Visual Stories Through Light';
